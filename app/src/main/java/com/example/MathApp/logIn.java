3092fb4d446363dc42f329cc35b46233c457d105
Schwartzblat <alon.ponch@gmail.com>
2020-10-30 11:52:32 +0200

    update log in mechanic

--- app/src/main/java/com/example/MathApp/logIn.java
+++ app/src/main/java/com/example/MathApp/logIn.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,9 +16,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class logIn extends AppCompatActivity {
-    private String [] users = new String[100];
-    private String [] passwords = new String[100];
-    String username ="", password="", status="no";
+
+    String username ="", password="";
+    int status = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,60 +30,43 @@ public class logIn extends AppCompatActivity {
         //get values
         username = ((TextView)findViewById(R.id.username)).getText().toString();
         password = ((TextView)findViewById(R.id.password)).getText().toString();
-        updateUsers();
-    }
-
-    public void updateUsers(){
-        FirebaseDatabase mFirebaseDatabase = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = mFirebaseDatabase.getReference();
-        new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-            }
-        };
-        myRef.child("Users").addValueEventListener(new ValueEventListener() {
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference().child("Users").child("user");
+        myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                showData(dataSnapshot);
+                User xuser;
+                for (DataSnapshot user : dataSnapshot.getChildren()) {
+                    xuser = user.getValue(User.class);
+                    if (xuser != null) {
+                        if (username.equals(xuser.username)&&password.equals(xuser.password)) {
+                            StatusIsOk();
+                            status = 1;
+                        }
+
+
+                    } else {
+                    }
+                }
+                Toast.makeText(getApplicationContext(), "invalid name or password", Toast.LENGTH_LONG).show();
+                ((TextView) findViewById(R.id.password)).setText("");
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
-            }
-        });
+            }});
     }
 
-    private void showData(DataSnapshot dataSnapshot) {
-        User xuser;
-        for (DataSnapshot user1 : dataSnapshot.child("user").getChildren()) {
-            xuser = user1.getValue(User.class);
-            if(xuser != null) {
-                if(username.equals(xuser.username) && password.equals(xuser.password)) {
-                    status = "ok";
-                    StatusIsOk();
-                }
 
-            }
-            else{
-            }
-        }
-        if(!status.equals("ok")) {
-            Toast.makeText(this, "invalid name or password", Toast.LENGTH_LONG).show();
-            ((TextView) findViewById(R.id.password)).setText("");
-        }
-    }
+
 
     public void StatusIsOk(){
         Intent i = new Intent(this, MainActivity.class);
         i.putExtra("username", username);
         i.putExtra("password", password);
         startActivity(i);
+        finish();
 
-    }
-    @Override
-    public void onBackPressed() {
-        moveTaskToBack(true);
     }
 
     public void launchSignUp(View v){
