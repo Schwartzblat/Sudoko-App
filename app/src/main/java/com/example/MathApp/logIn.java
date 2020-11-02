193c9e13bbe33ce98170b8fd2da0dcb91b874c91
Schwartzblat <alon.ponch@gmail.com>
2020-11-02 16:40:48 +0200

    add auth to log in

--- app/src/main/java/com/example/MathApp/logIn.java
+++ app/src/main/java/com/example/MathApp/logIn.java
@@ -10,6 +10,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -20,10 +24,13 @@ public class logIn extends AppCompatActivity {
     DataSnapshot data = null;
     String username ="", password="", email, phone;
     int status = 0;
+    FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        mAuth = FirebaseAuth.getInstance();
         DatabaseReference myRef = FirebaseDatabase.getInstance().getReference().child("Users").child("user");
         myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -49,6 +56,22 @@ public class logIn extends AppCompatActivity {
                     if (username.equals(xuser.username) && password.equals(xuser.password)) {
                         phone = xuser.phone;
                         email = xuser.email;
+                        mAuth.signInWithEmailAndPassword(email, password)
+                                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<AuthResult> task) {
+                                        if (task.isSuccessful()) {
+                                            StatusIsOk();
+                                        } else {
+                                            // If sign in fails, display a message to the user.
+                                            Toast.makeText(getApplicationContext(), "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                            // ...
+                                        }
+
+                                        // ...
+                                    }
+                                });
+
                         StatusIsOk();
                         status = 1;
                     }
